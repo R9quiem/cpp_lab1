0df2fa0ed7bbed35c775cdce99532d4797815634
efim <efimburachevsky@gmail.com>
2024-03-09 14:24:45 +0300

    completed task

--- cpp_lab_1.cpp
+++ cpp_lab_1.cpp
@@ -1,20 +1,71 @@
-﻿// cpp_lab_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
 
-#include <iostream>
+std::string solveEquation(const std::string& equationFile) {
+    std::ifstream inFile(equationFile);
+
+    if (!inFile.is_open()) {
+        std::cerr << "Ошибка. Не получается открыть файл с уравнением\n\n";
+        return "";
+    }
+
+    double a, b, c;
+
+    if (!(inFile >> a >> b >> c)) {
+        std::cerr << "Ошибка. Некорректные данные в файле.\n\n";
+        return "";
+    }
+
+    std::cout << "Данные успешно считаны с файла.\n\n";
+
+    if (a == 0) {
+        std::cerr << "Ошибка. В квадратном уравнении коэффициент 'a' не может быть равен 0.\n\n";
+        return "";
+    }
+    double D = b * b - 4 * a * c;
+
+    if (D < 0) {
+        std::cout << "Квадратное уравнение успешно решено.\n\n";
+        return "0 корней";
+    }
+    else if (D == 0) {
+        double root = -b / (2 * a);
+        std::cout << "Квадратное уравнение успешно решено.\n\n";
+        return "Один корень: " + std::to_string(root);
+    }
+    else {
+        double root1 = (-b + std::sqrt(D)) / (2 * a);
+        double root2 = (-b - std::sqrt(D)) / (2 * a);
+        std::cout << "Квадратное уравнение успешно решено.\n\n";
+        return "Два корня: " + std::to_string(root1) + ", " + std::to_string(root2);
+    }
+
+    std::cout << "Квадратное уравнение успешно решено.\n\n";
+}
+void writeAnswer(const std::string& resultFile, const std::string& roots) {
+    std::ofstream outFile(resultFile);
+
+    if (!outFile.is_open()) {
+        std::cerr << "Ошибка. Не получается открыть файл для записи результатов\n\n";
+        return;
+    }
+
+    outFile << roots;
+    outFile.close();
+    std::cout << "Результаты записаны в файл " << resultFile << " .\n\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "RU");
+    std::string equationFile = "equation.txt";
+    std::string resultFile = "result.txt";
+
+    std::string answer = solveEquation(equationFile);
+
+    writeAnswer(resultFile, answer);
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
